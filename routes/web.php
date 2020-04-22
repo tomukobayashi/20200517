8b4edd783f9c19e6aec1088ce9df3b53ba8a2116
tomukobayashi <scyoutec.34@gmail.com>
2020-04-22 11:37:54 +0900

    ミドルウェアの登録（個々のルート、グローバル、グループ）

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use App\Http\Middleware\HelloMiddleware;
+// use App\Http\Middleware\HelloMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('hello', 'HelloController@index')
-->middleware(HelloMiddleware::class);
+->middleware('hello');
+
+// ->middleware(HelloMiddleware::class);
