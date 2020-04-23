70f55fe702025aa3a0f931cbc75af834c6781dca
tomukobayashi <scyoutec.34@gmail.com>
2020-04-23 14:46:31 +0900

    DB_delete

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -62,4 +62,18 @@ public function update(Request $request)
    return redirect('/hello');
 }
 
+public function del(Request $request)
+{
+   $param = ['id' => $request->id];
+   $item = DB::select('select * from people where id = :id', $param);
+   return view('hello.del', ['form' => $item[0]]);
+}
+
+public function remove(Request $request)
+{
+   $param = ['id' => $request->id];
+   DB::delete('delete from people where id = :id', $param);
+   return redirect('/hello');
+}
+
 }
