5880a0f50bfbe3015ad766a7c228bcfb2e008cfc
tomukobayashi <scyoutec.34@gmail.com>
2020-04-23 15:05:32 +0900

    DB_where

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -84,8 +84,11 @@ public function remove(Request $request)
 //show
 public function show(Request $request)
 {
-    $id = $request->id;
-    $items = DB::table('people')->where('id', '<=', $id)->get();
+    $name = $request->name;
+    $items = DB::table('people')
+        ->where('name', 'like', '%' . $name . '%')
+        ->orWhere('mail', 'like', '%' . $name . '%')
+        ->get();
     return view('hello.show', ['items' => $items]);
 }
 
