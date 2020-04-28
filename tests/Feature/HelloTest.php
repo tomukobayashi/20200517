ca3813961549bf5b9929fa5fd366e72d032b77a2
tomukobayashi <scyoutec.34@gmail.com>
2020-04-28 16:27:46 +0900

    DBAcsess

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -25,20 +25,33 @@ class HelloTest extends TestCase
 
     public function testHello()
    {
-    $this->assertTrue(true);
+     // ダミーで利用するデータ
+     factory(User::class)->create([
+        'name' => 'AAA',
+        'email' => 'BBB@CCC.COM',
+        'password' => 'ABCABC',
+    ]);
+    factory(User::class, 10)->create();
 
-       $response = $this->get('/');
-       $response->assertStatus(200);
-      
-       $response = $this->get('/hello');
-       $response->assertStatus(302);
-      
-       $user = factory(User::class)->create();
-       $response = $this->actingAs($user)->get('/hello');
-       $response->assertStatus(200);
+    $this->assertDatabaseHas('users', [
+        'name' => 'AAA',
+        'email' => 'BBB@CCC.COM',
+        'password' => 'ABCABC',
+    ]);
 
-       $response = $this->get('/no_route');
-       $response->assertStatus(404);
+    // ダミーで利用するデータ
+    factory(Person::class)->create([
+        'name' => 'XXX',
+        'mail' => 'YYY@ZZZ.COM',
+        'age' => 123,
+    ]);
+    factory(Person::class, 10)->create();
+
+    $this->assertDatabaseHas('people', [
+        'name' => 'XXX',
+        'mail' => 'YYY@ZZZ.COM',
+        'age' => 123,
+    ]);
    }
 
 }
