82cedd704f5c5f66eb87c10dca89d1a819118531
tomukobayashi <scyoutec.34@gmail.com>
2020-05-17 14:08:09 +0900

    validator4

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -21,7 +21,6 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        // $items = DB::table('people')->simplePaginate(5);
         $user = Auth::user();
         if(!$request->sort) {
             $sort = 'id';
@@ -32,7 +31,6 @@ class HelloController extends Controller
                 ->simplePaginate(5);
         $param = ['items' => $items, 'sort' => $sort, 'user' => $user];
         return view('hello.index', $param);
-        // return view('hello.index', ['items' => $items]);
     }
 
    public function post(Request $request)
@@ -55,33 +53,26 @@ class HelloController extends Controller
            'mail' => $request->mail,
            'age' => $request->age,
        ];
-        DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
         DB::table('people')->insert($param);
-
        return redirect('/hello');
    }
 
 
    // update
    public function edit(Request $request)
-{
-//    $param = ['id' => $request->id];
-//    $item = DB::select('select * from people where id = :id', $param);
+    {
     $item = DB::table('people')
         ->where('id', $request->id)->first();
-//    return view('hello.edit', ['form' => $item[0]]);
     return view('hello.edit', ['form' => $item]);
-}
+    }
 
 public function update(HelloRequest2 $request)
-{
-   $param = [
-        //  'id' => $request->id,
+    {
+    $param = [
        'name' => $request->name,
        'mail' => $request->mail,
        'age' => $request->age,
    ];
-    // DB::update('update people set name =:name, mail = :mail, age = :age where id = :id', $param);
     DB::table('people')
         ->where('id', $request->id)
         ->update($param);
@@ -91,9 +82,6 @@ public function update(HelloRequest2 $request)
 //delete
 public function del(Request $request)
 {
-//    $param = ['id' => $request->id];
-//    $item = DB::select('select * from people where id = :id', $param);
-//    return view('hello.del', ['form' => $item[0]]);
     $item = DB::table('people')
         ->where('id', $request->id)->first();
     return view('hello.del', ['form' => $item]);
@@ -101,60 +89,9 @@ public function del(Request $request)
 
 public function remove(Request $request)
 {
-//    $param = ['id' => $request->id];
-//    DB::delete('delete from people where id = :id', $param);
-
     DB::table('people')
         ->where('id', $request->id)->delete();
    return redirect('/hello');
-}
-
-//show
-public function show(Request $request)
-{
-    $page = $request->page;
-   $items = DB::table('people')
-       ->offset($page * 3)
-       ->limit(3)
-       ->get();
-   return view('hello.show', ['items' => $items]);
-}
-
-public function rest(Request $request)
-{
-   return view('hello.rest');
-}
-
-public function ses_get(Request $request)
-{
-   $sesdata = $request->session()->get('msg');
-   return view('hello.session', ['session_data' => $sesdata]);
-}
-
-public function ses_put(Request $request)
-{
-   $msg = $request->input;
-   $request->session()->put('msg', $msg);
-   return redirect('hello/session');
-}
-
-public function getAuth(Request $request)
-{
-   $param = ['message' => 'ログインして下さい。'];
-   return view('hello.auth', $param);
-}
-
-public function postAuth(Request $request)
-{
-   $email = $request->email;
-   $password = $request->password;
-   if (Auth::attempt(['email' => $email,
-           'password' => $password])) {
-       $msg = 'ログインしました。（' . Auth::user()->name . '）';
-   } else {
-       $msg = 'ログインに失敗しました。';
-   }
-   return view('hello.auth', ['message' => $msg]);
 }
 
 }
